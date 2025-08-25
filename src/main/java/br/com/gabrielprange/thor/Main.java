2ce0bbd6fd5ef43f86440baf029ab7c2c659cff9
unknown <gabriel.fedprangevr@outlook.com>
2025-08-25 15:27:04 -0300

    classe ser

--- src/main/java/br/com/gabrielprange/thor/Main.java
+++ src/main/java/br/com/gabrielprange/thor/Main.java
@@ -2,6 +2,8 @@ package br.com.gabrielprange.thor;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        
     }
 }
---
+++ src/main/java/br/com/gabrielprange/thor/Ser.java
@@ -0,0 +1,19 @@
+package br.com.gabrielprange.thor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.print.DocFlavor.STRING;
+
+public class Ser {
+
+    private String nome;
+    private boolean vivo;
+    private String moradia;
+    private List<Artefato> artefatos =
+        new ArrayList<Artefato>();
+    private List<Ser> filhos =
+        new ArrayList<Ser>();
+    
+
+}
